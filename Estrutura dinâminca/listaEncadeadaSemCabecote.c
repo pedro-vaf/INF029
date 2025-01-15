d477c4e5abbe3b00394d5ceeabaa056fc1c5ec1e
Pedro Aquino <pedro.vaf@outlook.com>
2025-01-15 14:51:09 -0300

    Criação de procedimento de remoção no inicio e em algum valor específico

--- Estrutura dinâminca/listaEncadeadaSemCabecote.c
+++ Estrutura dinâminca/listaEncadeadaSemCabecote.c
@@ -75,6 +75,34 @@ void inserirMeioLista(No **lista){
     } else { printf("Erro ao alocar memória!\n"); }
 }
 
+/* Procedimento para inserir ordenado na lista*/
+void inserirOrdenado (No **lista){
+    int num;
+    printf("\nInsira o valor ordenado na lista -> ");
+    scanf("%d", &num);
+
+    No * temp, * novo = malloc(sizeof(No));
+
+    if (novo){
+        novo -> valor = num;
+        if (*lista == NULL){
+            novo -> proximo = NULL;
+            *lista = novo;
+        } else if (novo -> valor < (*lista) -> valor){
+            novo -> proximo = (*lista);
+            *lista = novo;
+        } else {
+            temp = *lista;
+            while (temp -> proximo && novo -> valor > temp -> proximo -> valor){
+                temp = temp -> proximo;
+            }
+            novo -> proximo = temp -> proximo;
+            temp -> proximo = novo;
+        }
+
+    } else { printf("Erro ao alocar memória!\n"); }
+}
+
 /* Procedimento para imprimir a lista encadeada */
 void imprimirLista(No *lista){
     printf("\n**** Elementos da lista ****\n");
@@ -86,6 +114,45 @@ void imprimirLista(No *lista){
     printf("\n**** Fim da lista ****\n");
 }
 
+/* Procedimento para remover primeiro elemento da lista */
+void removerLista (No **lista){
+    No * remover = NULL;
+    if (*lista != NULL){
+        remover = *lista;
+        printf("\nO valor %d foi removido da lista\n", remover -> valor);
+        *lista = remover -> proximo;
+        free(remover);
+    } else { printf("\nLista Vazia\n"); }
+}
+
+
+/* Procedimento para remover valor especifico da lista */
+void removerValorEspecificoLista(No **lista){
+    int num;
+    printf("\nInsira o valor ordenado na lista -> ");
+    scanf("%d", &num);
+
+    No * remover, * temp = NULL;
+    if (*lista != NULL){
+        if ((*lista) -> valor == num){
+            remover = *lista;
+            *lista = remover -> proximo;
+            free(remover);
+        } else {
+            temp = *lista;
+            while (temp -> proximo && temp -> proximo -> valor != num) {
+                temp = temp -> proximo;
+            }
+
+            if (temp -> proximo){
+                remover = temp -> proximo;
+                temp -> proximo = remover -> proximo;
+                printf("\nO valor %d foi removido da lista\n", remover -> valor);
+                free(remover);
+            }
+        }
+    } else { printf("\nLista Vazia\n"); }
+}
 
 /* Procedimento para liberar memória alocada para lista */
 void liberarLista(No **lista) {
@@ -102,8 +169,11 @@ int menuLista(int opcao){
     printf("1 - Inserir elemento no inicio da lista\n");
     printf("2 - Inserir elemento no meio da lista\n");
     printf("3 - Inserir elemento no final da lista\n");
-    printf("4 - Imprimir elementos da lista\n");
-    printf("5 - Sair\n");
+    printf("4 - Inserir elementos ordenado na lista\n");
+    printf("5 - Imprimir elementos da lista\n");
+    printf("6 - Remover o primeiro elemento da lista\n");
+    printf("7 - Remover um elemento específico na lista\n");
+    printf("8 - Sair\n");
     printf("Insira o numero referente -> ");
     scanf("%d", &opcao);
     return opcao;
@@ -129,7 +199,23 @@ int main(){
             break;
 
             case 4:
+                inserirOrdenado(&lista);
+            break;
+
+            case 5:
                 imprimirLista(lista);
+            break;
+
+            case 6:
+                removerLista(&lista);
+            break;
+
+            case 7:
+                removerValorEspecificoLista(&lista);
+            break;
+
+            case 8:
+                printf("\nSaindo...\n");
             break;
         
             default:
@@ -137,7 +223,7 @@ int main(){
             break;
         }
 
-    } while (opcao != 5);
+    } while (opcao != 8);
 
     liberarLista(&lista);
     return 0;
